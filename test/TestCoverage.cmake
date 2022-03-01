911b51556fbf3e338b25415189f92d5d1c8d0d10
Haofan Zheng <hzheng6@ucsc.edu>
2022-03-01 01:56:45 -0800

    Added MIT license header to all individual files

--- test/TestCoverage.cmake
+++ test/TestCoverage.cmake
@@ -1,3 +1,7 @@
+# Copyright 2022 Haofan Zheng
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
 
 ################################################################################
 # Test Coverage
